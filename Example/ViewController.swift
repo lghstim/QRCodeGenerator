f4deb1ac48ac62ee25dd117fc6ee029c4875e9a3
Tim Gorer <lghstim@gmail.com>
2016-11-11 14:10:48 -0800

    Generates address

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 import QRCode
 
+var id = "5"
+var website = "asapserver.herokupp.com/api/social/";
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var imageViewSmall1: UIImageView!
@@ -18,6 +21,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageViewMedium: UIImageView!
     @IBOutlet weak var imageViewLarge: UIImageView!
 
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,61 +29,12 @@ class ViewController: UIViewController {
         // large
         // default
         imageViewLarge.image = {
-            var qrCode = QRCode("http://github.com/aschuch/QRCode")!
+            var qrCode = QRCode(website+id)!
             qrCode.size = self.imageViewLarge.bounds.size
             qrCode.errorCorrection = .High
             return qrCode.image
         }()
         
-        // medium
-        // purple
-        imageViewMedium.image = {
-            var qrCode = QRCode("http://schuch.me")!
-            qrCode.size = self.imageViewMedium.bounds.size
-            qrCode.color = CIColor(rgba: "8e44ad")
-            return qrCode.image
-        }()
-        
-        // small
-        // red (inverted)
-        imageViewSmall1.image = {
-            var qrCode = QRCode("http://objc.at")!
-            qrCode.size = self.imageViewSmall1.bounds.size
-            qrCode.color = CIColor(rgba: "fff")
-            qrCode.backgroundColor = CIColor(rgba: "e74c3c")
-            return qrCode.image
-        }()
-        
-        // small
-        // green
-        imageViewSmall2.image = {
-            var qrCode = QRCode("http://apple.com")!
-            qrCode.size = self.imageViewSmall1.bounds.size
-            qrCode.color = CIColor(rgba: "16a085")
-            qrCode.backgroundColor = CIColor(rgba: "000")
-            return qrCode.image
-        }()
-        
-        // small
-        // orange
-        imageViewSmall3.image = {
-            var qrCode = QRCode("http://example.com")!
-            qrCode.size = self.imageViewSmall1.bounds.size
-            qrCode.color = CIColor(rgba: "c0392b")
-            qrCode.backgroundColor = CIColor(rgba: "f1c40f")
-            return qrCode.image
-            }()
-
-        
-        // small
-        // blue
-        imageViewSmall4.image = {
-            var qrCode = QRCode("http://example.com")!
-            qrCode.size = self.imageViewSmall1.bounds.size
-            qrCode.color = CIColor(rgba: "2980b9")
-            return qrCode.image
-        }()
-
     }
 
 }
